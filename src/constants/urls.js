924c435fa8566c7cbeb8d7e13dad65345cfa9e82
avivharuzi <avivharuzi@gmail.com>
2018-05-13 16:04:54 +0300

    update store

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,1 +1,5 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'http://localhost:3000'
+
+export const LOGIN_URL = '/auth/login'
+export const REGISTER_URL = '/auth/regiser'
+export const CHECK_TOKEN_URL = '/auth/check'
